4f3587ea5a135887b267e719b7a0876cf0a3661e
faizan uddin <faizan.uddin121@gmail.com>
2021-09-02 13:51:47 -0500

    React app with team page

--- src/main/java/io/faizanuddin/ipldashboard/controller/TeamController.java
+++ src/main/java/io/faizanuddin/ipldashboard/controller/TeamController.java
@@ -3,11 +3,13 @@ package io.faizanuddin.ipldashboard.controller;
 import io.faizanuddin.ipldashboard.model.Team;
 import io.faizanuddin.ipldashboard.repository.MatchRepository;
 import io.faizanuddin.ipldashboard.repository.TeamRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
 
